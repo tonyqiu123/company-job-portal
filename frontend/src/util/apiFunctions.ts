49a15c8bfb0fe378129e9603dd6b455b9db6f7ad
tonyqiu123 <tonyqiu12345@gmail.com>
2023-07-08 15:00:28 -0400

    debugged pdf upload;

--- frontend/src/util/apiFunctions.ts
+++ frontend/src/util/apiFunctions.ts
@@ -1,6 +1,6 @@
 // api.ts
-const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://company-job-portal-production.up.railway.app/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://company-job-portal-production.up.railway.app/api';
 
 async function handleErrorResponse(response: Response) {
   try {
